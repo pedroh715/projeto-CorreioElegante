50c3f66bc6d55e98eed7cfe2f4046bdd2bfc3f86
Pedro Grandizoli <pedrohenriquecontato04@gmail.com>
2022-01-07 21:09:27 -0400

    criando primeira tela

---
+++ correioElegante/src/pages/Start/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { 
+  Text, 
+  View, 
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+/* IMAGENS */
+import logo from '../../assets/logo.png';
+import imageTop from '../../assets/imageTop.png'
+
+const Start = () => {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.container}>
+        <Image source={imageTop}></Image>
+      </View>
+      
+      <View style={[styles.container, { 
+        borderTopRightRadius: 35,
+        borderTopLeftRadius: 35,
+        backgroundColor: '#fff'
+      }]}>
+        <Text style={[styles.textTitle, {
+          marginBottom: 17,
+          lineHeight: 35,
+          marginTop: 50
+        }]}>
+          Surpreenda seu amor</Text>
+
+        <Text style={[styles.textTitle, {
+            fontSize: 18,
+            color: '#8d8d8d',
+            lineHeight: 30
+        }]}>
+          Envie mensagens e presentes incríveis</Text>
+
+          <TouchableOpacity style={styles.button}>
+            <LinearGradient 
+            colors={['#E06C88', '#B73058']} 
+            style={[styles.button, {marginTop: 0}]}
+            useAngle={true}
+            angle={130}
+            >
+            <Text style={[styles.textButton]}>COMEÇAR</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+            
+      </View>
+    </View>
+  ); 
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: "#B73058",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textTitle: {
+    fontSize: 37,
+    fontFamily: 'Poppins',
+    color: '#2f394b',
+    fontWeight: '700',
+    width: 300,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+
+  textButton: {
+    fontSize: 20,
+    fontFamily: 'Roboto',
+    color: '#FFF',
+    fontWeight: '700',
+  },
+
+  button: {
+    width: 295,
+    height: 60,
+    backgroundColor: '#000',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default Start; 
